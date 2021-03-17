26734e554efac1a95f8b4f8a1249c55705486b5d
Mikhail Tseluiko <mikhail.tseluiko@binary-studio.com>
2021-03-17 17:42:16 +0200

    JSON RE: fix date format RE

---
+++ reverse_engineering/helpers/adaptJsonSchema.js
@@ -0,0 +1,53 @@
+const mapJsonSchema = require('./mapJsonSchema');
+
+const handleDate = field => {
+	return Object.assign({}, field, {
+		type: 'datetime'
+	});
+};
+
+const handleTime = field => {
+	return Object.assign({}, field, {
+		type: 'datetime'
+	});
+};
+
+const handleDateTime = field => {
+	return Object.assign({}, field, {
+		type: 'datetime'
+	});
+};
+
+
+const handleStringFormat = field => {
+	const { format, ...fieldData } = field;
+
+	switch(format) {
+		case 'date':
+			return handleDate(fieldData);
+		case 'time':
+			return handleTime(fieldData);
+		case 'date-time':
+			return handleDateTime(fieldData);
+		default:
+			return field;
+	};
+};
+
+
+const adaptType = field => {
+	const type = field.type;
+
+	if (type === 'string') {
+		return handleStringFormat(field);
+	}
+
+	return field;
+};
+
+
+const adaptJsonSchema = (_, jsonSchema) => {
+	return mapJsonSchema(_)(jsonSchema, adaptType);
+};
+
+module.exports = { adaptJsonSchema };
