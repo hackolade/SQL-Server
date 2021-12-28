0e694ef15339356c56229bcd47379fff8897a360
Lench Volodymyr <volodymyr.lench@binary-studio.com>
2021-12-28 16:04:11 +0200

    add ssh settigns

--- reverse_engineering/connectionState.js
+++ reverse_engineering/connectionState.js
@@ -1,13 +1,22 @@
 const { getConnectionClient } = require('./databaseService/databaseService');
+const sshHelper = require('./helpers/sshHelper');
 
 const stateInstance = {
 	_client: null,
+	_sshTunnel: null,
 	getClient: () => this._client,
 	setClient: async (connectionInfo, attempts = 0) => {
+		if (connectionInfo.ssh && !this._sshTunnel) {
+			const sshData = await sshHelper.connectViaSsh(connectionInfo);
+			connectionInfo = sshData.info;
+			this._sshTunnel = sshData.tunnel;			
+		}
+
 		try {
 			this._client = await getConnectionClient(connectionInfo)
 		} catch (error) {
-			const isEncryptedConnectionToLocalInstance = error.message.includes('self signed certificate') && connectionInfo.encryptConnection;
+			const encryptConnection = connectionInfo.encryptConnection === undefined || Boolean(connectionInfo.encryptConnection);
+			const isEncryptedConnectionToLocalInstance = error.message.includes('self signed certificate') && encryptConnection;
 
 			if (isEncryptedConnectionToLocalInstance && attempts <= 0) {
 				return stateInstance.setClient({
@@ -19,7 +28,14 @@ const stateInstance = {
 			throw error;
 		}
 	},
-	clearClient: () => this._client = null,
+	clearClient: () => {
+		this._client = null;
+
+		if (this._sshTunnel) {
+			this._sshTunnel.close();
+			this._sshTunnel = null;
+		}
+	},
 }
 
 module.exports = stateInstance;
