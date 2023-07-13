170bdb600eac91715b467af2027916500b9d7ca3
Danylo Strelets <danylo.strelets@hackolade.com>
2023-07-13 14:47:24 +0300

    HCK-3553: standardize record sampling limitation

--- reverse_engineering/helpers/getOptionsFromConnectionInfo.js
+++ reverse_engineering/helpers/getOptionsFromConnectionInfo.js
@@ -1,12 +1,9 @@
 const getOptionsFromConnectionInfo = connectionInfo => ({
 	includeEmptyCollection: connectionInfo.includeEmptyCollection,
 	isFieldOrderAlphabetic: connectionInfo.fieldInference.active === 'alphabetical',
-	rowCollectionSettings: {
-		isAbsoluteValue: connectionInfo.recordSamplingSettings.active === 'absolute',
-		value: connectionInfo.recordSamplingSettings.active === 'absolute'
-			? connectionInfo.recordSamplingSettings.absolute.value
-			: connectionInfo.recordSamplingSettings.relative.value,
-	},
+	recordSamplingSettings: {
+		...connectionInfo.recordSamplingSettings,
+	}
 });
 
 module.exports = getOptionsFromConnectionInfo;
